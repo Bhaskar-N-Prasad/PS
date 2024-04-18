45022a0f31f0afc8f840e4ea0e800ba48e24cdb9
Bhaskar-N-Prasad <bhaskars.co@gmail.com>
2024-04-18 17:44:38 +0530

    Variables

--- Fan.py
+++ Fan.py
@@ -13,8 +13,18 @@ f1 = Fan()
 # print(f1.numOfBlades)
 # f1.rotate()
 f2 = f1
-print(f1)
-print(f2)
-print(id(f1))
-print(id(f2))
-print(f1 is f2)
+# print(f1)
+# print(f2)
+# print(id(f1))
+# print(id(f2))
+# print(f1 is f2)
+
+a = 10
+b = 10
+print(a is b)
+print(id(b))
+b = 20
+print(a is b)
+print(id(b))
+print(id(a))
+print(a)
