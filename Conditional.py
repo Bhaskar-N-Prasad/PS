4d3524d1712e2a245c1c9ab98e0a448815c9fb98
Bhaskar-N-Prasad <bhaskars.co@gmail.com>
2024-04-19 17:21:59 +0530

    Conditional

---
+++ Conditional.py
@@ -0,0 +1,8 @@
+a = int(input("Enter Value 1\n"))
+b = int(input("Enter Value 2\n"))
+if a > b:
+    print("a is greater than b")
+elif a < b:
+    print("a is less than b")
+else:
+    print("a is equal to b")
