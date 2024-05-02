6fb753533c25bf6974facb7e37b0917f2d7229e3
Bhaskar-N-Prasad <bhaskars.co@gmail.com>
2024-05-02 16:50:31 +0530

    SwapCase

--- caseConvert.py
+++ caseConvert.py
@@ -8,3 +8,22 @@ for i in data:
     else:
         str += i
 print(str)
+
+
+data = input("Enter a string\n")
+str = ""
+for i in data:
+    if ord(i)>=97 and ord(i)<=122:
+        str += chr(ord(i)-32) 
+    else:
+        str += i
+print(str)
+
+data = input("Enter a string\n")
+str = ""
+for i in data:
+    if ord(i)>=97 and ord(i)<=122:
+        str += chr(ord(i)-32) 
+    else:
+        str += chr(ord(i)+32)
+print(str)
---
+++ swapCase.py
@@ -0,0 +1,17 @@
+# str = input("Enter a string\n")
+# str1 = str.upper()
+# str2 = str.lower()
+# str3 = str.swapcase()
+# print(str)
+# print(str1)
+# print(str2)
+# print(str3)
+
+
+str1 = "Rama"
+str2 = "Sita"
+# print(str1 + str2)
+# print(str1 - str2)
+# print(str1 * str2)
+# print(str1 * 3)
+print(str1 / str2)
